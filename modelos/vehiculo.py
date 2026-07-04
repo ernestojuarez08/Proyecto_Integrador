45efa498bd8912ca0f04a8013a76bab65231d7e7
Alex Cervantes <Alex@MacBook-Air.local>
2026-07-04 17:21:14 -0600

    Estructura inicial del modelo Vehiculo

--- modelos/vehiculo.py
+++ modelos/vehiculo.py
@@ -10,3 +10,24 @@ class Vehiculo:
 
     def __str__(self):
         return f"{self.matricula} | {self.propietario} | {self.tipo}"
+    
+    def to_dict(self):
+        return {
+            "matricula": self.matricula,
+            "propietario": self.propietario,
+            "tipo": self.tipo,
+            "modelo": self.modelo,
+            "color": self.color,
+            "activo": self.activo
+    }
+
+@classmethod
+def from_dict(cls, datos):
+        return cls(
+            matricula=datos["matricula"],
+            propietario=datos["propietario"],
+            tipo=datos["tipo"],
+            modelo=datos["modelo"],
+            color=datos["color"],
+            activo=datos["activo"]
+        )
